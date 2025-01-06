a52844456b81a61188dfba8d9023f997e116109b
Muhammed Farzin <muhammedfarzink5@gmail.com>
2025-01-07 00:16:43 +0530

    Implement admin login functionality with form validation and error handling

--- client/src/pages/admin/AdminLogin.tsx
+++ client/src/pages/admin/AdminLogin.tsx
@@ -1,9 +1,67 @@
+import { useEffect, useState } from "react";
 import GloomLinkLogo from "../../assets/images/GloomLink-Logo.svg";
 import LoginIllustrationDark from "../../assets/images/Login-Illustration-Dark.svg";
 import FormBox from "../../components/FormBox";
 import InputBox from "../../components/InputBox";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { useNavigate } from "react-router-dom";
+import {
+  AdminAuthState,
+  logout,
+  setAuthAdmin,
+  TokensState,
+} from "../../redux/reducers/auth";
+import { LoginFormType, validateLoginForm } from "../user/formValidations";
+import axios, { AxiosError } from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
 const AdminLogin: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const adminData = useSelector((state: RootState) => state.auth.adminData);
+  const [formData, setFormData] = useState<LoginFormType>({
+    username: "",
+    password: "",
+  });
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  useEffect(() => {
+    const adminAccessToken = localStorage.getItem("adminAccessToken");
+    if (adminData && adminAccessToken) {
+      navigate("/admin");
+    } else {
+      dispatch(logout({ type: "admin" }));
+    }
+  }, [adminData]);
+
+  const handleOnLogin: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+
+    const isValidated = validateLoginForm(formData, setErrorMessage);
+
+    if (!isValidated) return;
+
+    try {
+      const response = await axios.post(BASE_URL + "/admin/login", formData);
+      const adminData = response.data.userData as AdminAuthState;
+      const tokens = response.data.tokens as TokensState;
+
+      dispatch(setAuthAdmin({ adminData, tokens }));
+      navigate("/admin");
+    } catch (error) {
+      if (error instanceof AxiosError && error.response) {
+        setErrorMessage(error.response.data.message);
+      } else setErrorMessage("Something went wrong");
+    }
+  };
+
+  const handleOnChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <div className="py-3 px-4 md:py-6 md:px-14">
@@ -20,9 +78,23 @@ const AdminLogin: React.FC = () => {
         </div>
 
         <div className="w-full md:w-1/2 px-4 my-auto text-center">
-          <FormBox title="Admin Login">
-            <InputBox placeholder="Username" type="text" />
-            <InputBox placeholder="Password" type="password" />
+          <FormBox
+            title="Admin Login"
+            errorMessage={errorMessage}
+            onSubmit={handleOnLogin}
+          >
+            <InputBox
+              placeholder="Username"
+              type="text"
+              name="username"
+              onChange={handleOnChange}
+            />
+            <InputBox
+              placeholder="Password"
+              type="password"
+              name="password"
+              onChange={handleOnChange}
+            />
             <button className="btn btn-primary border w-full">Login</button>
           </FormBox>
         </div>
