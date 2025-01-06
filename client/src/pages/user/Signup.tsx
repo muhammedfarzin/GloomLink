341a0ea7e34a9a140fe8cce2460e9fff6b746939
Muhammed Farzin <muhammedfarzink5@gmail.com>
2025-01-06 23:11:23 +0530

    Implement signup functionality with form validation; integrate Redux for user state management; enhance error handling and navigation

--- client/src/pages/user/Signup.tsx
+++ client/src/pages/user/Signup.tsx
@@ -4,14 +4,76 @@ import GloomLinkLogo from "../../assets/images/GloomLink-Logo.svg";
 import SignUpIllustrationDark from "../../assets/images/SignUp-Illustration-Dark.svg";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import DropDownBox from "../../components/DropDownBox";
+import axios, { AxiosError } from "axios";
+import { validateSignUpForm, type SignUpFormType } from "./formValidations";
+import {
+  logout,
+  setAuthUser,
+  type TokensState,
+  type UserAuthState,
+} from "../../redux/reducers/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
 const maxDate = new Date(new Date().setFullYear(new Date().getFullYear() - 5));
 const Signup = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userData = useSelector((state: RootState) => state.auth.userData);
+  const [formData, setFormData] = useState<SignUpFormType>({
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: "",
+    mobile: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [dob, setDob] = useState<Date>();
   const [gender, setGender] = useState<string>();
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  useEffect(() => {
+      const accessToken = localStorage.getItem("accessToken");
+      if (accessToken && userData && userData.status !== "blocked") {
+        navigate("/");
+      } else {
+        dispatch(logout({ type: "user" }));
+      }
+    }, [userData]);
+
+  const handleOnSignup: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    try {
+      const isValidated = validateSignUpForm(formData, setErrorMessage);
+      if (!isValidated) return;
+
+      const response = await axios.post(BASE_URL + "/user/signup", {
+        ...formData,
+        dob: dob?.toISOString(),
+        gender: gender,
+      });
+
+      const userData = response.data.userData as UserAuthState;
+      const tokens = response.data.tokens as TokensState;
+
+      dispatch(setAuthUser({ userData, tokens }));
+      navigate("/signup/verify");
+    } catch (error) {
+      if (error instanceof AxiosError && error.response) {
+        setErrorMessage(error.response.data.message);
+      } else setErrorMessage("Something went wrong");
+    }
+  };
+
+  const handleOnChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   return (
     <>
@@ -28,15 +90,50 @@ const Signup = () => {
           />
         </div>
 
-        <div className="w-full md:w-1/2 px-4 my-auto text-center">
-          <FormBox title="Sign Up">
+        <div className="w-full md:w-1/2 px-4 my-auto text-center md:mt-[-2.5rem]">
+          <FormBox
+            title="Sign Up"
+            errorMessage={errorMessage}
+            onSubmit={handleOnSignup}
+          >
             <div className="flex gap-2">
-              <InputBox placeholder="First name" type="text" />
-              <InputBox placeholder="Last name" type="text" />
+              <InputBox
+                value={formData.firstname}
+                name="firstname"
+                onChange={handleOnChange}
+                placeholder="First name"
+                type="text"
+              />
+              <InputBox
+                value={formData.lastname}
+                name="lastname"
+                onChange={handleOnChange}
+                placeholder="Last name"
+                type="text"
+              />
             </div>
 
-            <InputBox placeholder="Username" type="text" />
-            <InputBox placeholder="Email" type="email" />
+            <InputBox
+              value={formData.username}
+              name="username"
+              onChange={handleOnChange}
+              placeholder="Username"
+              type="text"
+            />
+            <InputBox
+              value={formData.email}
+              name="email"
+              onChange={handleOnChange}
+              placeholder="Email"
+              type="email"
+            />
+            <InputBox
+              value={formData.mobile}
+              name="mobile"
+              onChange={handleOnChange}
+              placeholder="Mobile"
+              type="number"
+            />
 
             <div className="flex gap-2">
               <DropDownBox
@@ -66,10 +163,24 @@ const Signup = () => {
               />
             </div>
 
-            <InputBox placeholder="Password" type="password" />
-            <InputBox placeholder="Confirm password" type="password" />
+            <InputBox
+              value={formData.password}
+              name="password"
+              onChange={handleOnChange}
+              placeholder="Password"
+              type="password"
+            />
+            <InputBox
+              value={formData.confirmPassword}
+              name="confirmPassword"
+              onChange={handleOnChange}
+              placeholder="Confirm password"
+              type="password"
+            />
 
-            <button className="btn btn-primary border w-full">Sign Up</button>
+            <button type="submit" className="btn btn-primary border w-full">
+              Sign Up
+            </button>
           </FormBox>
 
           <Link
