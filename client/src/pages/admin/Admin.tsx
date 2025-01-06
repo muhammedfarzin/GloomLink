24f4626ee13d85316c7fdacb129ff3681312d12e
Muhammed Farzin <muhammedfarzink5@gmail.com>
2025-01-07 00:16:26 +0530

    Implement admin authentication checks and logout functionality; update Redux state management for admin data

--- client/src/pages/admin/Admin.tsx
+++ client/src/pages/admin/Admin.tsx
@@ -1,10 +1,24 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import AdminSideMenuBar from "./components/AdminSideMenuBar";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { logout } from "../../redux/reducers/auth";
 
 const Admin: React.FC = () => {
+  const navigate = useNavigate();
   const location = useLocation();
+  const dispatch = useDispatch();
+  const adminData = useSelector((state: RootState) => state.auth.adminData);
   const [selectedValue, setSelectedValue] = useState("home");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("adminAccessToken");
+    if (!adminData || !accessToken) {
+      dispatch(logout({ type: "admin" }));
+      navigate("/admin/login");
+    }
+  }, [adminData]);
 
   useEffect(() => {
     const selected = location.pathname.split("/").pop();
--- client/src/redux/reducers/auth.ts
+++ client/src/redux/reducers/auth.ts
@@ -13,22 +13,34 @@ export interface UserAuthState {
   dob?: Date;
 }
 
+export interface AdminAuthState {
+  username: string;
+}
+
 export interface TokensState {
   accessToken: string;
-  refreshToken?: string;
+  refreshToken: string;
 }
 
 interface UserAuthWithTokenState {
   userData: UserAuthState;
+  tokens: Omit<TokensState, "refreshToken"> &
+    Partial<Pick<TokensState, "refreshToken">>;
+}
+
+interface AdminAuthWithTokenState {
+  adminData: AdminAuthState;
   tokens: TokensState;
 }
 
 interface AuthState {
   userData: UserAuthState | null;
+  adminData: AdminAuthState | null;
 }
 
 const initialState: AuthState = {
   userData: null,
+  adminData: null,
 };
 
 const authSlice = createSlice({
@@ -44,16 +56,28 @@ const authSlice = createSlice({
 
       state.userData = userData;
     },
+    setAuthAdmin: (state, action: PayloadAction<AdminAuthWithTokenState>) => {
+      const { adminData, tokens } = action.payload;
+
+      localStorage.setItem("adminAccessToken", tokens.accessToken);
+      localStorage.setItem("adminRefreshToken", tokens.refreshToken);
+
+      state.adminData = adminData;
+    },
     logout: (state, action: PayloadAction<{ type: "admin" | "user" }>) => {
       const type = action.payload.type;
       if (type === "user") {
         state.userData = null;
         localStorage.removeItem("accessToken");
         localStorage.removeItem("refreshToken");
+      } else if (type === "admin") {
+        state.adminData = null;
+        localStorage.removeItem("adminAccessToken");
+        localStorage.removeItem("adminRefreshToken");
       }
     },
   },
 });
 
-export const { setAuthUser, logout } = authSlice.actions;
+export const { setAuthUser, setAuthAdmin, logout } = authSlice.actions;
 export default authSlice.reducer;
