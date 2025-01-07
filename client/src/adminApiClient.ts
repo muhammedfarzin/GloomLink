96dd72849f8fa5941a73e7487c50f643bbee8d7a
Muhammed Farzin <muhammedfarzink5@gmail.com>
2025-01-07 14:03:41 +0530

    Add admin API client and update user API client for token refresh handling

--- client/src/adminApiClient.ts
+++ client/src/adminApiClient.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
+import store from "./redux/store"; // Adjust the import according to your store file location
+import { logout } from "./redux/reducers/auth"; // Adjust the import according to your actions file location
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL as string + "/user";
+const BASE_URL = (import.meta.env.VITE_API_BASE_URL as string) + "/admin";
 
-const apiClient = axios.create({
+const adminApiClient = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-apiClient.interceptors.request.use(
+adminApiClient.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem("accessToken");
+    const accessToken = localStorage.getItem("adminAccessToken");
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
@@ -23,7 +24,7 @@ apiClient.interceptors.request.use(
   }
 );
 
-apiClient.interceptors.response.use(
+adminApiClient.interceptors.response.use(
   (response) => {
     return response;
   },
@@ -32,18 +33,19 @@ apiClient.interceptors.response.use(
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
-        const refreshToken = localStorage.getItem("refreshToken");
-        const response = await axios.post(BASE_URL + "/auth/refresh", {
+        const refreshToken = localStorage.getItem("adminRefreshToken");
+        const response = await axios.post(BASE_URL + "/user/auth/refresh", {
           token: refreshToken,
         });
         const newAccessToken = response.data.accessToken;
         localStorage.setItem("accessToken", newAccessToken);
-        apiClient.defaults.headers.common[
+        adminApiClient.defaults.headers.common[
           "Authorization"
         ] = `Bearer ${newAccessToken}`;
         originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
-        return apiClient(originalRequest);
+        return adminApiClient(originalRequest);
       } catch (refreshError) {
+        store.dispatch(logout({ type: "admin" }));
         return Promise.reject(refreshError);
       }
     }
@@ -51,4 +53,4 @@ apiClient.interceptors.response.use(
   }
 );
 
-export default apiClient;
+export default adminApiClient;
