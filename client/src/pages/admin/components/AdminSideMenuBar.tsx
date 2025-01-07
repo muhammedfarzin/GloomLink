f4b07b7a9a744ab62629ec1cc681c77c4e86555d
Muhammed Farzin <muhammedfarzink5@gmail.com>
2025-01-07 15:01:42 +0530

    Added logout button for admin panel

--- client/src/pages/admin/components/AdminSideMenuBar.tsx
+++ client/src/pages/admin/components/AdminSideMenuBar.tsx
@@ -1,17 +1,45 @@
+import { useDispatch } from "react-redux";
 import MenuButton from "../../../components/MenuButton";
 import SideMenuBar from "../../../components/SideMenuBar";
+import { logout } from "../../../redux/reducers/auth";
 
 interface AdminSideMenuBarProps {
   selected?: string;
 }
 
-const AdminSideMenuBar: React.FC<AdminSideMenuBarProps> = ({selected}) => {
+const AdminSideMenuBar: React.FC<AdminSideMenuBarProps> = ({ selected }) => {
+  const dispatch = useDispatch();
+
   return (
     <SideMenuBar homePath="/admin">
-      <MenuButton to="/admin" text="Dashboard" selected={selected === 'admin'} />
-      <MenuButton to="/admin/users" text="Users" selected={selected === 'users'} />
-      <MenuButton to="/admin/posts" text="Posts" selected={selected === 'posts'} />
-      <MenuButton to="/admin/subscriptions" text="Subscriptions" selected={selected === 'subscriptions'} />
+      <MenuButton
+        to="/admin"
+        text="Dashboard"
+        selected={selected === "admin"}
+      />
+      <MenuButton
+        to="/admin/users"
+        text="Users"
+        selected={selected === "users"}
+      />
+      <MenuButton
+        to="/admin/posts"
+        text="Posts"
+        selected={selected === "posts"}
+      />
+      <MenuButton
+        to="/admin/subscriptions"
+        text="Subscriptions"
+        selected={selected === "subscriptions"}
+      />
+      <MenuButton
+        to="/logout"
+        text="Logout"
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch(logout({ type: "admin" }));
+        }}
+      />
     </SideMenuBar>
   );
 };
