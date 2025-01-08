7a1eec86aa64d29141f8326367cc3002e82a41ac
Muhammed Farzin <muhammedfarzink5@gmail.com>
2025-01-08 14:16:24 +0530

    Add profileImage prop to ProfileImage component for customizable avatar

--- client/src/components/ProfileImage.tsx
+++ client/src/components/ProfileImage.tsx
@@ -1,13 +1,21 @@
 import ProfileAvatar from "../assets/icons/Profile-Avatar.svg";
 
 interface ProfileImageProps {
+  profileImage?: string;
   isOnline?: boolean;
 }
 
-const ProfileImage: React.FC<ProfileImageProps> = ({ isOnline = false }) => {
+const ProfileImage: React.FC<ProfileImageProps> = ({
+  profileImage,
+  isOnline = false,
+}) => {
   return (
     <div className="bg-slate-300 w-1/4 min-w-8 max-w-16 rounded-full mr-2 relative">
-      <img src={ProfileAvatar} alt="Avatar" className="w-full" />
+      <img
+        src={profileImage || ProfileAvatar}
+        alt="Profile"
+        className="w-full"
+      />
       {isOnline ? (
         <div className="w-2 h-2 bg-green-700 rounded-full absolute bottom-1 right-0" />
       ) : null}
